537410a78ba58d9be022ff35f1e53af1bddce32f
Khaironz <roberto.tec27@gmail.com>
2026-01-13 16:19:12 -0300

    Pet Age Logic

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,39 @@
-from app.main import get_human_age
+import pytest
+import app.main as main
+from typing import List
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+
+        (27, 0, [2, 0]),
+        (28, 0, [3, 0]),
+        (0, 28, [0, 2]),
+        (0, 29, [0, 3]),
+
+        (0, 0, [0, 0]),
+        (1, 1, [0, 0]),
+        (100, 100, [21, 17]),
+    ]
+)
+def test_get_human_age(
+        cat_age: int,
+        dog_age: int,
+        expected: List[int]
+) -> None:
+    assert main.get_human_age(cat_age, dog_age) == expected
+
+
+def test_return_type() -> None:
+    result: List[int] = main.get_human_age(
+        20,
+        20
+    )
+    assert isinstance(result, list)
+    assert len(result) == 2
+    assert all(isinstance(x, int) for x in result)
